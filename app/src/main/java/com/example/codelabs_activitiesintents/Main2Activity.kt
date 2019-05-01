cb9db5840c57dca79c7b9651e84b22051f7bb86c
Haroon khan <haroonkhan9426@gmail.com>
2019-05-01 13:26:50 +0500

    Basic Intents plus intents extra functionality added

--- app/src/main/java/com/example/codelabs_activitiesintents/Main2Activity.kt
+++ app/src/main/java/com/example/codelabs_activitiesintents/Main2Activity.kt
@@ -2,11 +2,19 @@ package com.example.codelabs_activitiesintents
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.EditText
+import android.widget.TextView
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        var textView: TextView = findViewById(R.id.textView)
+        val extras = intent.extras ?: return
+        val message: String = extras.getString("message")
+        textView.text = message
+
     }
 }
--- app/src/main/java/com/example/codelabs_activitiesintents/MainActivity.kt
+++ app/src/main/java/com/example/codelabs_activitiesintents/MainActivity.kt
@@ -1,12 +1,25 @@
 package com.example.codelabs_activitiesintents
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val editText: EditText = findViewById(R.id.editText)
+        val button: Button = findViewById(R.id.button)
+
+        button.setOnClickListener {
+            var intent = Intent(this, Main2Activity::class.java)
+            val message: String = editText.text.toString()
+            intent.putExtra("message", message)
+            startActivity(intent)
+        }
     }
 }
